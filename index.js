56b5aa9fea96482bd5558bd034d40adb93c98504
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-10-17 23:03:41 +0200

    Api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //http://www.omdbapi.com/?apikey=7a4561cc&t=star+wars
 
 
-fetch ("http://www.omdbapi.com/?apikey=7a4561cc")
+fetch ("http://www.omdbapi.com/?t=star+wars&type=movie&v=2&apikey=7a4561cc")
     .then(res => res.json())
     .then(data => console.log(data))
