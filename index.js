763085ee118021f08454ee3ce870fb354dcf81a7
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-10-25 12:15:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ fetch ("http://www.omdbapi.com/?t=star+wars&type=movie&apikey=7a4561cc")
 srcBtn.addEventListener('click', function(){
     fetch (`http://www.omdbapi.com/?s=${titleImput.value}&type=movie&apikey=7a4561cc`)
     .then(res => res.json())
-    .then(data => console.log(data.Search))
+    .then(data => {
+        //console.log(data.Response)
+        const movieListArray = data.Search
+        //console.log(movieListArray)
+        if(data.Response === 'False'){
+            console.log("No movie with this title. try again!")
+        } else{
+            console.log(movieListArray)
+            let movieList = ""
+            movieListArray.filter(movie =>{
+                movieList += ``
+            })
+        }
+    })
 })
