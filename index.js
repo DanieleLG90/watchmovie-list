d625a58a4a56a5bf93304ed1c16e471090e45e3b
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-10-26 19:28:08 +0200

    testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const titleImput = document.getElementById("titleImput")
 const srcBtn = document.getElementById("srcBtn")
+const list = document.getElementById("list")
 
 /*
 fetch ("http://www.omdbapi.com/?t=star+wars&type=movie&apikey=7a4561cc")
@@ -13,20 +14,32 @@ srcBtn.addEventListener('click', function(){
     .then(data => {
 
         const movieListArray = data.Search
-        let movieList = ""
         
         if(data.Response === 'False'){
-            console.log("No movie with this title. try again!")
+            list.textContent = "No movie with this title. try again!"
+            //console.log("No movie with this title. try again!")
         } else{
-            console.log(movieListArray)
-            //movieListArray.filter(listMovieFilter(movie))
+            //console.log(movieListArray)
+            
+           movieListArray.filter(movie => {
+
+
+                return fetch(`http://www.omdbapi.com/?t=${movie.Title}&type=movie&apikey=7a4561cc`)
+                    .then(res => res.json())
+                    .then(data => {
+                        //console.log(data)
+                        let movieList = ""
+                        movieList += `<img src="${movie.Poster}" alt="movie poster">`
+                        console.log(movieList)
+                        list.innerHTML = movieList
+                    }) 
+            })
+
+        
         }
     })
-    function listMovieFilter (movie){
-        movieList += ``
-        console.log('working')
-    }
     
+   
 })
 
 //creare un altro fetch, che lavora su ogni singolo titolo attraverso il Filter.
