7750c6af4b91409743930634c98c0b6573daefb7
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-11-16 14:28:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,42 +15,44 @@ srcBtn.addEventListener('click', function(){
             list.innerHTML = `<h3 class="noResultMsg">No movie with this title. try again!</h3>`
         
         } else{
-            //console.log(movieListArray)
-            let movieList =''
-            movieListArray.forEach(movie =>{
-                fetch(`https://www.omdbapi.com/?t=${movie.Title}&type=movie&apikey=7a4561cc`)
-                    .then(res => res.json())
-                    .then(data =>{
-                        //console.log(data)
-                        
-                        movieList += `  
-                                <div class="movieCard">
-                                    <img class="moviePoster" src="${data.Poster}" alt="movie Poster">
-                                    <div class="infoContainer">
-                                        <div class="movieInfo1">
-                                            <h2 class="movieTitle">${data.Title}</h2>
-                                            <h3 class="movieRate">${data.imdbRating}</h3>
-                                        </div>
-                                        <div class="movieInfo2">
-                                            <div class="movieStyleContainer">
-                                                <span class="movieDuration">${data.Runtime}</span>
-                                                <span class="movieType">${data.Genre}</span>
-                                            </div>
-                                            <button class="addBtn"><img class="addMovieImg" src="movies-add.svg" alt="">Watchlist</button>
-                                        </div>
-                                        <p class="plot">${data.Plot}</p>
-                                    </div>
-                                </div>`
-                               
-                        list.innerHTML = movieList
-
-                    }) 
-            })
+            displayMovie(movieListArray)
         }
     })
  
 })
 
 
+function displayMovie(movieListed){
+    let movieList =''
+    movieListed.forEach(movie =>{
+        fetch(`https://www.omdbapi.com/?t=${movie.Title}&type=movie&apikey=7a4561cc`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+                
+                movieList += `  
+                        <div class="movieCard">
+                            <img class="moviePoster" src="${data.Poster}" alt="movie Poster">
+                            <div class="infoContainer">
+                                <div class="movieInfo1">
+                                    <h2 class="movieTitle">${data.Title}</h2>
+                                    <h3 class="movieRate">${data.imdbRating}</h3>
+                                </div>
+                                <div class="movieInfo2">
+                                    <div class="movieStyleContainer">
+                                        <span class="movieDuration">${data.Runtime}</span>
+                                        <span class="movieType">${data.Genre}</span>
+                                    </div>
+                                    <button class="addBtn"><img class="addMovieImg" src="movies-add.svg" alt="">Watchlist</button>
+                                </div>
+                                <p class="plot">${data.Plot}</p>
+                            </div>
+                        </div>`
+                       
+                list.innerHTML = movieList
+
+            }) 
+    })   
+}
 
 
