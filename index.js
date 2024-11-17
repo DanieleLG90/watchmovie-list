967cc9d2c876902fb467d5ce7be94b88990648bf
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-11-17 14:43:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ srcBtn.addEventListener('click', function(){
 
 
 function displayMovie(movieListed){
+
     let movieList =''
     movieListed.forEach(movie =>{
         fetch(`https://www.omdbapi.com/?t=${movie.Title}&type=movie&apikey=7a4561cc`)
@@ -43,7 +44,7 @@ function displayMovie(movieListed){
                                         <span class="movieDuration">${data.Runtime}</span>
                                         <span class="movieType">${data.Genre}</span>
                                     </div>
-                                    <button class="addBtn"><img class="addMovieImg" src="movies-add.svg" alt="">Watchlist</button>
+                                    <button class="addBtn" onclick="myFunction(${data.Title})" ><img class="addMovieImg" src="movies-add.svg" alt="">Watchlist</button>
                                 </div>
                                 <p class="plot">${data.Plot}</p>
                             </div>
@@ -52,7 +53,44 @@ function displayMovie(movieListed){
                 list.innerHTML = movieList
 
             }) 
-    })   
+            
+    }) 
+}
+function myFunction(movie) {
+    console.log(movie)
 }
 
-
+/*
+function displayMovie(movieListed) {
+    let movieList = '';
+  
+    movieListed.forEach(movie => {
+      fetch(`https://www.omdbapi.com/?t=${movie.Title}&type=movie&apikey=7a4561cc`)
+        .then(res => res.json())
+        .then(data => {
+          movieList += `
+            <div class="movieCard">
+              <img class="moviePoster" src="${data.Poster}" alt="movie Poster">
+              <div class="infoContainer">
+                <button class="addBtn" data-movie-title="${data.Title}">
+                  <img class="addMovieImg" src="movies-add.svg" alt="">Watchlist
+                </button>
+                </div>
+            </div>
+          `;
+        });
+    });
+  
+    // Add event listener to the container element (replace 'list' with your container ID)
+    document.getElementById('list').addEventListener('click', (event) => {
+      if (event.target.classList.contains('addBtn')) {
+        const movieTitle = event.target.dataset.movieTitle;
+        myFunction(movieTitle);
+      }
+    });
+  }
+  
+  function myFunction(movie) {
+    console.log(movie);
+  }
+    */
